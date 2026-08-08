e8e2ba791c84e5969209eb935674d6203e5de51b
Min Thant Tun <138033284+FightaHackathon@users.noreply.github.com>
2026-08-08 13:31:07 +0630

    feat: register cached game and source status APIs

--- gamepulse/web_api/app.py
+++ gamepulse/web_api/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from gamepulse.web_api.routes.games import router as games_router
 from gamepulse.web_api.routes.jobs import router as jobs_router
+from gamepulse.web_api.routes.status import router as status_router
 
 app = FastAPI(title="GamePulse API", version="1")
+app.include_router(games_router)
+app.include_router(status_router)
 app.include_router(jobs_router)
 
 
